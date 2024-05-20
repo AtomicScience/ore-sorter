3117f230471b84b2b4b3e662a638cdf46bee1493
AtomicScience <dmitry.greblyuk@mail.ru>
2024-05-20 17:13:14 +0300

    /dist removed

--- dist/folder/fileToImport.lua
+++
@@ -1,6 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-____exports.helloWorld = function()
-    print("Hello, World!")
-end
-return ____exports
--- dist/main.lua
+++
@@ -1,6 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-local ____exports = {}
-local ____fileToImport = require("folder.fileToImport")
-local helloWorld = ____fileToImport.helloWorld
-helloWorld(nil)
-return ____exports
